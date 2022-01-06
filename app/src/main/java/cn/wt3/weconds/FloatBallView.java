f6daa490f6f981bc8dde6e5640068e9a8e2a2d00
qqdm <ghcony@qq.com>
2022-01-07 00:14:58 +0800

    项目第一个版本上传

---
+++ app/src/main/java/cn/wt3/weconds/FloatBallView.java
@@ -0,0 +1,218 @@
+package cn.wt3.weconds;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class FloatBallView {
+
+
+    private Context context;
+
+    private int height = 0;
+
+    private int width = 0;
+
+    public static FloatBallView floatView2;
+
+
+    private TextView condstext = null;
+
+
+    public static FloatBallView getInstance(Context context) {
+        if (floatView2 == null) {
+            floatView2 = new FloatBallView(context);
+        }
+        return floatView2;
+    }
+
+    public FloatBallView(Context c) {
+
+        this.context = c;
+
+    }
+
+    private WindowManager wm;
+
+    private View view;// 浮动按钮
+
+    WindowManager.LayoutParams params;
+
+    /**
+     * 添加悬浮View
+     *
+     * @Cony
+     */
+
+    public void createFloatView() {
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.home_floatview, null);
+        }
+
+
+        wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        height = wm.getDefaultDisplay().getHeight();
+        width = wm.getDefaultDisplay().getWidth();
+
+        params = new WindowManager.LayoutParams();
+        params.type = WindowManager.LayoutParams.TYPE_BASE_APPLICATION;// 所有程序窗口的“基地”窗口，其他应用程序窗口都显示在它上面。
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        params.format = PixelFormat.TRANSLUCENT;// 不设置这个弹出框的透明遮罩显示为黑色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        }
+
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        int screenWidth = context.getResources().getDisplayMetrics().widthPixels;
+        int screenHeight = context.getResources().getDisplayMetrics().heightPixels;
+        params.y = screenHeight - height / 3;//设置距离底部高度为屏幕三分之一
+        params.x = screenWidth;
+        view.setBackgroundColor(Color.TRANSPARENT);
+        view.setVisibility(View.VISIBLE);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            // 触屏监听
+            float lastX, lastY;
+            int oldOffsetX, oldOffsetY;
+            int tag = 0;// 悬浮球 所需成员变量
+
+            @Override
+
+            public boolean onTouch(View v, MotionEvent event) {
+                final int action = event.getAction();
+                float x = event.getX();
+                float y = event.getY();
+                if (tag == 0) {
+                    oldOffsetX = params.x; // 偏移量
+                    oldOffsetY = params.y; // 偏移量
+                }
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    lastX = x;
+                    lastY = y;
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                    params.x += (int) (x - lastX) / 3; // 减小偏移量,防止过度抖动
+                    params.y += (int) (y - lastY) / 3; // 减小偏移量,防止过度抖动
+                    tag = 1;
+                    wm.updateViewLayout(view, params);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    int newOffsetX = params.x;
+                    int newOffsetY = params.y;
+                    // 只要按钮一动位置不是很大,就认为是点击事件
+                    if (Math.abs(oldOffsetX - newOffsetX) <= 20
+                            && Math.abs(oldOffsetY - newOffsetY) <= 20) {
+                        if (l != null) {
+                            l.onClick(view);
+                        }
+                    } else {
+                        if (params.x < width / 2) {
+                            params.x = 0;
+                        } else {
+                            params.x = width;
+                        }
+                        wm.updateViewLayout(view, params);
+                        tag = 0;
+                    }
+                }
+                return true;
+            }
+        });
+        try {
+            wm.addView(view, params);
+        } catch (Exception e) {
+
+        }
+
+        /*floatView2.onFloatViewClick(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //这边是点击悬浮按钮的响应事件
+                Toast.makeText(context, "点击了悬浮球", Toast.LENGTH_LONG);
+            }
+        });*/
+    }
+
+    /**
+     * 点击浮动按钮触发事件，需要override该方法
+     */
+
+    private View.OnClickListener l;
+
+    public void onFloatViewClick(View.OnClickListener l) {
+        this.l = l;
+    }
+
+    /**
+     * 将悬浮View从WindowManager中移除，需要与createFloatView()成对出现
+     */
+
+    public void removeFloatView() {
+        if (wm != null && view != null) {
+            wm.removeViewImmediate(view);
+//          wm.removeView(view);//不要调用这个，WindowLeaked
+            view = null;
+            wm = null;
+        }
+    }
+
+    /**
+     * 隐藏悬浮View
+     */
+
+    public void hideFloatView() {
+        if (wm != null && view != null && view.isShown()) {
+            view.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * 显示悬浮View
+     */
+
+    public void showFloatView() {
+        if (wm != null && view != null && !view.isShown()) {
+            view.setVisibility(View.VISIBLE);
+        }
+    }
+
+    //数据更新测试方法
+    public void updatatest(String ss) {
+        if (wm != null && view != null && view.isShown()) {
+            condstext = (TextView) view.findViewById(R.id.condstext);
+            condstext.setText(ss);
+        }
+    }
+
+    public void updateViewLayout() {
+        if (wm != null) {
+            int screenWidth = (int) 480;
+            int screenHeight = (int) 720;
+            if (screenWidth == 0) {
+                screenWidth = context.getResources().getDisplayMetrics().widthPixels;
+            }
+
+            if (screenHeight == 0) {
+                screenHeight = context.getResources().getDisplayMetrics().heightPixels;
+                params.y = screenHeight - height / 3;//设置距离底部高度为屏幕三分之一
+            } else {
+                params.y = screenHeight;
+            }
+            params.x = screenWidth;
+            wm.updateViewLayout(view, params);
+        }
+
+    }
+
+}
+
---
+++ app/src/main/java/cn/wt3/weconds/MainActivity.java
@@ -0,0 +1,233 @@
+package cn.wt3.weconds;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Message;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int msgKey1 = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new TimeThread().start();
+
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        TextView devsite = findViewById(R.id.devsite);
+        Button safe = findViewById(R.id.safe);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //检查是否已经授予权限
+                    if (!Settings.canDrawOverlays(MainActivity.this)) {
+                        //若未授权则请求权限
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                        intent.setData(Uri.parse("package:" + getPackageName()));
+                        startActivityForResult(intent, 0);
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "Weconds已经获取到悬浮框权限！", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        //显示悬浮球
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("点击了打开悬浮框按钮");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //检查是否已经授予权限
+                    if (!Settings.canDrawOverlays(MainActivity.this)) {
+                        //若未授权则请求权限
+                        Toast.makeText(getApplicationContext(),
+                                "请先获取悬浮框权限！不授权我怎么显示出来？", Toast.LENGTH_LONG).show();
+                    } else {
+                        FloatBallView.getInstance(MainActivity.this).createFloatView();
+                        Toast.makeText(getApplicationContext(),
+                                "秒数框已经出现！可以拖动，自动吸附边缘！", Toast.LENGTH_LONG).show();
+
+
+                    }
+                }
+            }
+        });
+
+        //隐藏悬浮球
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FloatBallView.getInstance(MainActivity.this).removeFloatView();
+                Toast.makeText(getApplicationContext(),
+                        "秒数框已隐藏！可以再次开启！", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //点击开发者文字跳转到官网
+        devsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Uri uri = Uri.parse("http://wt3.cn/");
+                final Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(it);
+            }
+        });
+
+        safe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showAlterDialog();
+
+//                之前测试了浮窗数据写入的
+//                double sjs = Math.random();
+//                String sjsss = String.valueOf(sjs);
+//                FloatBallView.getInstance(MainActivity.this).updatatest(sjsss);
+
+
+            }
+        });
+
+        //这边是点击悬浮按钮的响应事件
+
+        FloatBallView.getInstance(MainActivity.this).onFloatViewClick(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "我比较喜欢靠边站，拖哪都一样！", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public class TimeThread extends Thread {
+        @Override
+        public void run() {
+            do {
+                try {
+                    //线程每次休息100毫秒，
+                    Thread.sleep(100);
+                    Message msg = new Message();
+                    msg.what = msgKey1;
+                    mHandler.sendMessage(msg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (true);
+        }
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case msgKey1:
+                    //mTime.setText(getTime());
+                    FloatBallView.getInstance(MainActivity.this).updatatest(getTime());
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    //获得当前年月日时分秒星期
+    public String getTime() {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        String mHour = String.valueOf(c.get(Calendar.HOUR_OF_DAY));//时
+        if (c.get(Calendar.HOUR_OF_DAY) < 10) {
+            mHour = "0" + mHour;
+        }
+        String mMinute = String.valueOf(c.get(Calendar.MINUTE));//分
+        if (c.get(Calendar.MINUTE) < 10) {
+            mMinute = "0" + mMinute;
+        }
+        String mSecond = String.valueOf(c.get(Calendar.SECOND));//秒
+        if (c.get(Calendar.SECOND) < 10) {
+            mSecond = "0" + mSecond;
+        }
+        return mHour + ":" + mMinute + ":" + mSecond;
+    }
+
+    private void showAlterDialog() {
+        final AlertDialog.Builder alterDiaglog = new AlertDialog.Builder(MainActivity.this);
+
+        alterDiaglog.setTitle("Weconds隐私保护声明");//文字
+        alterDiaglog.setMessage("Weconds获取悬浮窗权限之后将时分秒小浮窗显示在其他App上层，并且以0.1秒为周期获取手机的系统时间。(当前版本号 V3.0)\n\n" +
+                "Weconds不涉及任何联网行为，也不会读取任何无关文件，请放心使用。\n\n" +
+                "如有问题请联系邮箱：ghcony@qq.com\n\n" +
+                "本项目已开源，如有兴趣请前往 https://github.com/qqdm 获取项目。\n\n" +
+                "By：王探长工作室(官网：wt3.cn)");//提示消息
+        //积极的选择
+        alterDiaglog.setPositiveButton("复制邮箱地址", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("clip", "ghcony@qq.com");
+                cm.setPrimaryClip(clip);
+                Toast.makeText(MainActivity.this, "期待您的反馈！", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //消极的选择
+        alterDiaglog.setNegativeButton("复制GitHub地址", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("clip", "https://github.com/qqdm");
+                cm.setPrimaryClip(clip);
+                Toast.makeText(MainActivity.this, "请粘贴到浏览器访问", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //中立的选择
+        alterDiaglog.setNeutralButton("同意声明", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "感谢您的支持，请您放心使用！", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //显示
+        alterDiaglog.show();
+    }
+
+
+}
+
+
+
